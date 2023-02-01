37e17f347291067e348396578e97ab3e45dcb756
Bikram Karki <karkibikram353@gmail.com>
2023-02-01 18:12:07 +1030

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,28 @@
+# Body Mass Index (BMI) Calculator
+
+
+name = input("What's your name? ")
+weight = float(input("Enter your weight in pounds: "))
+height = float(input("Enter your height in inches: "))
+
+BMI = (weight * 703)/(height * height)
+
+print(f"Your BMI is {BMI}")
+
+
+
+if BMI > 0:
+    if BMI < 18.5:
+        print(name + ", You are underweight.")
+    elif BMI <= 24.9:
+        print(name + ", You are normal weight.")
+    elif BMI <=29.9:
+        print(name + ", You are overweight.")
+    elif BMI <=34.9:
+        print(name + ", You are obese.")
+    elif BMI <= 39.9:
+        print(name + ", You are severely obese.")
+    else:
+        print("You are morbidly obese.")
+else:
+    print("Enter valid input.")
